c7d54ca9aa45a13e34de9cbb6660f16c569bb99c
Binguni <binguni@example.com>
2023-10-15 15:37:59 +0530

    Add codes for calculating the product

--- Numbers.py
+++ Numbers.py
@@ -1,3 +1,6 @@
 numbers <- 1:10
 total <- sum(numbers)
 print(paste("The sum of the numbers from 1 to 10 is:", total))
+
+product <- prod(numbers)
+print(paste("The product of the numbers from 1 to 10 is:", product))
